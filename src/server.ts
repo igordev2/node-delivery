1a42f400eed7697ed8ef3dd99a3dcd111428cd5d
sadnode <igor_csns_s2@hotmail.com>
2021-12-21 17:24:14 -0300

    Create Clients

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.get('/', (request, response) => {
     return response.json({
