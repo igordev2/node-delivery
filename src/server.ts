08194c48dfc77bdb8f56116b5496a7e18f2bc938
sadnode <igor_csns_s2@hotmail.com>
2021-12-21 18:12:27 -0300

    handle asynchronous exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 const app = express();
@@ -7,10 +8,17 @@ app.use(express.json());
 
 app.use(routes);
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Hello World!'
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: 'Internal server error'
     });
-});
+})
 
 app.listen(3000, () => console.log(`Server is running 🚀`)); 
