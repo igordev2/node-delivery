140109dde4e1837b8b6a8db33b08ad45569b1991
sadnode <igor_csns_s2@hotmail.com>
2021-12-21 18:41:02 -0300

    Create deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { AuthenticateClientController } from './modules/accounts/authenticateUser/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController';
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+
 const authenticateClientController = new AuthenticateClientController();
+
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post('/clients/', createClientController.handle);
 
 routes.post('/authenticate/', authenticateClientController.handle);
+
+routes.post('/deliveryman/', createDeliverymanController.handle);
 
 export { routes };
