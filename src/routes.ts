f1a6d0e9b4f1ac52abefaa334630aece9c1e1fed
sadnode <igor_csns_s2@hotmail.com>
2021-12-21 21:38:55 -0300

    Authenticate deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AuthenticateDeliverymanController } from './modules/accounts/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { AuthenticateClientController } from './modules/accounts/authenticateUser/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
@@ -6,15 +7,15 @@ import { CreateDeliverymanController } from './modules/deliveryman/useCases/crea
 const routes = Router();
 
 const createClientController = new CreateClientController();
-
 const authenticateClientController = new AuthenticateClientController();
 
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
 routes.post('/clients/', createClientController.handle);
-
-routes.post('/authenticate/', authenticateClientController.handle);
+routes.post('/client/authenticate/', authenticateClientController.handle);
 
 routes.post('/deliveryman/', createDeliverymanController.handle);
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 export { routes };
